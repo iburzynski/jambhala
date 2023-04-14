130d989e6ae695463a519c53f0171938cf0c326d
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-04-14 10:50:54 -0400

    Bump cardano-node to 1.35.7

--- flake.nix
+++ flake.nix
@@ -45,18 +45,19 @@
       packages.default = flake.packages."jambhala:exe:jamb";
     });
 
-  # nixConfig = {
-  #   extra-trusted-substituters = [
-  #     "https://cache.iog.io"
-  #     "https://cache.zw3rk.com"
-  #   ];
-  #   extra-trusted-public-keys = [
-  #     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-  #     "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
-  #   ];
-  #   allow-import-from-derivation = true;
-  #   accept-flake-config = true;
-  #   substitute = true;
-  # };
+  nixConfig = {
+    bash-prompt = "\\[\\e[0;92m\\][\\[\\e[0;92m\\]Jambhala:\\[\\e[0;92m\\]\\w\\[\\e[0;92m\\]]\\[\\e[0;92m\\]$ \\[\\e[0m\\]";
+    extra-trusted-substituters = [
+      "https://cache.iog.io"
+      "https://cache.zw3rk.com"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+    ];
+    allow-import-from-derivation = true;
+    accept-flake-config = true;
+    substitute = true;
+  };
 
 }
