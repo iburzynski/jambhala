c0bd09b7830aa81e6186917cf4b3a670e1a62978
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-05-01 23:33:03 -0400

    Remove nixConfig from flake

--- flake.nix
+++ flake.nix
@@ -64,18 +64,17 @@
         packages.default = flake.packages."jambhala:exe:jamb";
       });
 
-  nixConfig = {
-    bash-prompt = "\\[\\e[0;92m\\][\\[\\e[0;92m\\]Jambhala:\\[\\e[0;92m\\]\\w\\[\\e[0;92m\\]]\\[\\e[0;92m\\]$ \\[\\e[0m\\]";
-    extra-trusted-substituters = [
-      "https://cache.iog.io"
-      "https://cache.zw3rk.com"
-    ];
-    extra-trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
-    ];
-    allow-import-from-derivation = true;
-    substitute = true;
-  };
-
+  # nixConfig = {
+  #   bash-prompt = "\\[\\e[0;92m\\][\\[\\e[0;92m\\]Jambhala:\\[\\e[0;92m\\]\\w\\[\\e[0;92m\\]]\\[\\e[0;92m\\]$ \\[\\e[0m\\]";
+  #   extra-substituters = [
+  #     "https://cache.iog.io"
+  #     "https://cache.zw3rk.com"
+  #   ];
+  #   extra-trusted-public-keys = [
+  #     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+  #     "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+  #   ];
+  #   allow-import-from-derivation = true;
+  #   accept-flake-config = true;
+  # };
 }
